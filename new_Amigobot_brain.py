87e41cf65ea68a31fb6ecb2a82efabc5f65a00d7
boomcan90 <boomcan90@users.noreply.github.com>
2015-04-08 22:54:13 +0800

    Update new_Amigobot_brain.py

--- new_Amigobot_brain.py
+++ new_Amigobot_brain.py
@@ -2,229 +2,430 @@ import math
 import libdw.sm as sm
 from soar.io import io
 import libdw.gfx as gfx
-import libdw.util as util
+import urllib2
 import libdw.sonarDist as sonarDist
-from libdw.boundarySM import *
-import copy
+import libdw.search as Search
 
-## BRAIN SM
+######################################################################
+#
+#            Brain SM
+#
+######################################################################
 
-desiredFront = 0.7
-desiredRight = 0.7
-speedForward = 0.5
+testing = True
+ 
+# Forward threshold and side threshold
+dDesired = 0.7
+dRight = 0.7
 tolerance = 0.1
-factor = 45
-upperBound = factor + tolerance
-lowerBound = factor - tolerance
-turnAngle = math.pi/4
-counterT = 30
-juncFwd = 40
-maxRangeOfSensor = 2.5
-rvelLimitHigh = 1
-rvelLimitLow = -1
-didTurn = False 
-juncOverflow = False
 
+turningAngle = math.pi / 4
+
+maxSensorReading = 3
+maxReadingOverrideForTurning = 5
+rVelUpperLimit = 1
+rVelLowerLimit = -1
+hadWallBefore = False
+timerW = 150
+
+
+if testing:
+    counterTurn = 21
+    counterJunction = 35
+    counterRotation = 42
+    counterExitRotation = 15
+    k1 = 30
+    k2 = -29.77
+else:
+    counterTurn = 31
+    counterJunction = 28
+    counterRotation = 63
+    counterExitRotation = 20
+    k1 = 100
+    k2 = -97.95
 
 class Sensor(sm.SM):
-
+ 
     def getNextValues(self, state, inp):
-        sensorValueList = inp.sonars
-        rightFunction = sonarDist.getDistanceRight(sensorValueList)
+        sensors = inp.sonars
         if state is None:
-            state = [sensorValueList[5], sensorValueList[5], sensorValueList[5]]
-            median = state
+            state = [0, 0]
+            state[0] = [[i, i, i] for i in sensors]
+            state[1] = [sonarDist.getDistanceRight(sensors)] * 3
+            medianSensorRight = state[1]
+            medianSensors = sensors
         else:
-            state.pop(0)
-            state.append(sensorValueList[5])
-            median = copy.deepcopy(state)  
-            median.sort()
-        rightMidValue = median[1]
-        return state, (rightFunction, rightMidValue, sensorValueList)
+            medianSensors = []
+            for idx, sensor in enumerate(state[0]):
+                # each sensor sort it
+                state[0][idx].pop(0)
+                state[0][idx].append(sensors[idx])
+                temp = state[0][idx][:]
+                temp.sort()
+                medianSensors.append(temp[1])
+            state[1].pop(0)
+            state[1].append(sonarDist.getDistanceRight(sensors))
+            medianSensorRight = state[1][:]
+            medianSensorRight.sort()
+        return state, (medianSensorRight[1], medianSensors)
 
-
-class wallFollower(sm.SM):
-
-    state = None
-
-
+ 
+class WallFollower(sm.SM):
+    # start with None to get first sensor reading
+    startState = None
+ 
+    # should move forward to X first
+    hadWallBefore = False
+ 
+    # right and left depending on orientation and path list
+    right = 2
+    left = 0
+ 
+    # generated path that the robot should follow
+    path = []
+    currentMoveCommand = ('Default', 'Default')
+    nextMoveCommand = ('Default', 'Default')
+ 
     def getNextValues(self, state, inp):
-        if state is None:
-            state = ("Fwd", 0, 0, inp)
-            return (state, io.Action(fvel = 0.0, rvel = 0.0))
-            # state = ( move, timer, counter, (rightFunction, rightMidValue, sensorValueList))
-            #           ----------------------                                               -
-            #                                                 THIS IS INP
-
-        if not state is None:
-            rightFunctionNow = inp[0]
-            rightFunctionAgo = state[3][0]
-            rightMidValueNow = inp[1]
-            rightMidValueAgo = state[3][1]
+        # state = (movement, previousReadings, counter, timer)
+        # state = ("Fwd, (0.5, [0.5, 0.5, 0.5, 0.5, 0.5, 0.5], average), 0, 0)
+        if state is not None:
+            rightFormulaNow = inp[0]
+            rightFormulaAgo = state[1][0]
             counter = state[2]
-            timer = state[1]
+            sensors = inp[1]
+            timer = state[3]
+            rightFrontSensorAgo = state[1][1][3]
+            rightDiagonalSensorAgo = state[1][1][4]
+            rightSensorAgo = state[1][1][5]
+            leftSensorAgo = state[1][1][0]
+            leftFrontSensorAgo = state[1][1][2]
             move = state[0]
-            prevSensors = state[3][2]
-            nowSensors = inp[2]
+            if rightFrontSensorAgo > leftFrontSensorAgo + 2:
+                previousFront = 0.9 * leftFrontSensorAgo
+            elif leftFrontSensorAgo > rightFrontSensorAgo + 2:
+                previousFront = 0.9 * rightFrontSensorAgo
+            else:
+                previousFront = (rightFrontSensorAgo + leftFrontSensorAgo) / 2.0
 
-        fvel = 0.2
+        if state is None:
+            state = ('Fwd', inp, 0, 0)
+            return (state, io.Action(fvel=0.0, rvel=0.0))
+ 
         rvel = 0.0
-        c3 = -2.5    
+        k3 = -2.0
+        fvel = 0.5
+ 
+        # Forward (F)
+        if move is 'Fwd':
 
-        ## MOVING FORWARD
 
-        if move is "Fwd":
-            c1 = 30.00
-            c2 = -29.77
-            val1 = desiredRight - rightFunctionNow
-            val2 = desiredRight - rightFunctionAgo
-
-            if rightFunctionAgo <= maxRangeOfSensor:
-                rvel = (c1 * val1) + (c2 * val2)
-                # if rvel > rvelLimitHigh:
-                #     rvel = turnAngle
-                # if rvel < rvelLimitLow: 
-                #     rvel = -turnAngle
-
-            ## Checking if there is a wall in front:
-
-            ## Checking previous set of sensors if there is a wall
-            if prevSensors[2]>prevSensors[3]+2:
-                prevFront = 0.9 * prevSensors[3]
-            elif prevSensors[2]+2 < prevSensors[3]:
-                prevFront = 0.9 * prevSensors[3]
+            e1 = dRight - rightFormulaNow
+            e2 = dRight - rightFormulaAgo
+ 
+            if (rightSensorAgo <= maxSensorReading and
+                    rightDiagonalSensorAgo <= maxSensorReading):
+                rvel = (k1 * e1) + (k2 * e2)
+ 
+            if wallIsInfront(previousFront):
+                if self.hadWallBefore is False:
+                    self.currentMoveCommand = self.path.pop(0)
+                    self.hadWallBefore = True
+                    if not (previousFront >= maxSensorReading):
+                        fvel = (k3 * (dDesired - previousFront))
+                    else:
+                        fvel = 0.8
+                state = ('Wait', inp, 0, 0)
             else:
-                prevFront = (prevSensors[2] + prevSensors[3]) / 2.0
+                if not (previousFront >= maxSensorReading):
+                    fvel = (k3 * (dDesired - previousFront))
+                else:
+                    fvel = 0.8
+                state = ('Fwd', inp, 0, 0)
+ 
+            print rightSensorAgo, leftSensorAgo
 
 
-            ## Checking current set of data if there is a wall - using 2 set of data so that there is no change of state due to phantom values.
-            if nowSensors[2]>nowSensors[3]+2:
-                nowFront = 0.9 * prevSensors[3]
-            elif nowSensors[2]+2 < nowSensors[3]:
-                nowFront = 0.9 * nowSensors[3]
-            else:
-                nowFront = (nowSensors[2] + nowSensors[3]) / 2.0
-
-
-            if frontWallPresent(prevFront) and frontWallPresent(nowFront):
-                fvel = 0.0
-                state = ("Wait", 0, 0, inp)
-            
-
-            if (junctionCheck(nowSensors[0:4]) or junctionCheck(nowSensors[1:5]) or junctionCheck(nowSensors[2:6])): 
-                fvel = 0.0
+            if (rightSensorAgo >= maxReadingOverrideForTurning or leftSensorAgo >= maxReadingOverrideForTurning) \
+                    and self.hadWallBefore:
+                print "No Junc"
                 rvel = 0.0
-                state = ("Junc", 0, 0, inp)
-            else:
-                # print "Previous front is", prevFront, "\n"
-                # print "Now front is ", nowFront, "\n"
-                # print inp[2]
-                state = ("Fwd", 0, 0, inp)
-
-
-
-
-        if move is "Junc":
-            # if stickout(nowSensors):
-            #     state = ("3wayJunc", 0, 0, inp)
-            # elif stickoutLeft(nowSensors, prevSensors):
-            #     state = ("2wayLeftStraightJunc", 0, 0)
-            # elif stickoutRight(nowSensors, prevSensors):
-            #     state = ("2wayRightStraightJunc")
-
-
-
-
-
-        ## Waiting for 15 seconds. 
-
-        if move is "Wait":
-            if timer <= 150:
                 fvel = 0.0
-                rvel = 0.0
-                timer +=1
-                state = ("Wait", timer, 0, inp)
-            else:
-                print "Stopping"
-                state = ("End", timer, 0, inp)
-        
+                counter += 1
+                state = ('Fwd', inp, counter, 0)
+                if counter >= 5:
+                    state = ('Junc', inp, 0, 0)
+ 
 
-
-        if move is "turnLeft":
+        if move is 'turnLeft':
             counter += 1
             fvel = 0.0
-            rvel = turnAngle
-            if counter >= counterT:
-                state = ("Fwd", 0, 0, inp)
-            else: 
-                state = ("turnLeft", 0, counter, inp)
+            rvel = turningAngle
+            state = ('turnLeft', inp, counter, 0)
+            if counter >= counterTurn:
+                state = ('exitJuncion', inp, 0, 0)
+ 
+        if move is "turnRight":
+            counter += 1
+            fvel = 0.0
+            rvel = -turningAngle
+            state = ("turnRight", inp, counter, 0)
+            if counter >= counterTurn:
+                state = ("exitJunction", inp, 0, 0)
+ 
+        # Junction (J)
+        if move is "Junc":
 
-
-
-        ## End of movement. 
-
-        if move is "End":
+            # uh why did i do this again?
+            if not (rightSensorAgo > maxSensorReading or leftSensorAgo > maxSensorReading):
+                fvel = 0.2
+                state = ("Junc", inp, 0, 0)
+            else:
+                fvel = 0.2
+                counter += 1
+                state = ("Junc", inp, counter, 0)
+                if counter >= counterJunction:
+                    #  just to check if its a junction
+                    if self.currentMoveCommand[1][0] == "Junc":
+                        self.currentMoveCommand = self.nextMoveCommand[:]
+                        if not len(self.path) > 0:
+                            self.nextMoveCommand = (0, 0)
+                        else:
+                            self.nextMoveCommand = self.path.pop(0)
+                        if self.currentMoveCommand[0] == self.right:
+                            state = ("turnRight", inp, 0, 0)
+                            self.right = (self.currentMoveCommand[0] + 1) % 4
+                            self.left = (self.currentMoveCommand[0] - 1) % 4
+                        elif self.currentMoveCommand[0] == self.left:
+                            state = ("turnLeft", inp, 0, 0)
+                            self.right = (self.currentMoveCommand[0] + 1) % 4
+                            self.left = (self.currentMoveCommand[0] - 1) % 4
+                        else:
+                            state = ("exitJunction", inp, 0, 0)
+                    else:
+                        state = ("exitJunction", inp, 0, 0)
+ 
+        if move is "exitJunction":
+            counter += 1
+            fvel = 0.2
+            state = ("exitJunction", inp, counter, 0)
+            if counter >= counterJunction + 20:
+                state = ("Fwd", inp, 0, 0)
+ 
+        if move is "Wait":
+            if timer <= timerW:
+                timer += 1
+                fvel = 0.0
+                rvel = 0.0
+                state = ("Wait", inp, 0, timer)
+            else:
+                state = ("rotate", inp, 0, 0)
+ 
+        # Rotate (R) - Rotate around.
+        if move is "rotate":
+            counter += 1
+            fvel = 0.0
+            rvel = turningAngle
+            if counter >= counterRotation:
+                if len(self.path) > 0:
+                    # Check for next command, set current Command
+                    print self.path[0]
+                    self.currentMoveCommand = self.path.pop(0)  # (1, "Junc)
+                    self.nextMoveCommand = self.path.pop(0)  # (2, 'J1')
+                    self.right = (self.currentMoveCommand[0] + 1) % 4
+                    self.left = (self.currentMoveCommand[0] - 1) % 4
+                    print self.path[0]
+                    state = ("exitRotation", inp, 0, 0)
+                else:
+                    # probably end of route
+                    state = ("Halt", inp, 0, 0)
+            else:
+                state = ("rotate", inp, counter, 0)
+ 
+        # Exit Rotation (ER) - Exit the rotation state
+        if move is "exitRotation":
+            e1 = dRight - rightFormulaNow
+            e2 = dRight - rightFormulaAgo
+            rvel = (k1 * e1) + (k2 * e2)
+            counter += 1
+            state = ("exitRotation", inp, counter, 0)
+            if counter >= counterExitRotation:
+                state = ("Fwd", inp, 0, 0)
+ 
+        if move is "Halt":
             fvel = 0.0
             rvel = 0.0
-            state = ("End", 0, 0, inp)
+            state = ("Halt", inp, 0, 0)
 
-        if move is "Fwd1s":
-            fvel = 0.2
-            rvel = 0
-            counter = 1
-            state = ("Junc", 0, counter, inp)
-
-
-
-
-        return (state, io.Action(fvel = fvel, rvel = rvel))
-
-
-
+        print move
+        print self.hadWallBefore
+        print self.path[0]
+        return (state, io.Action(fvel=fvel, rvel=rvel))
+ 
+ 
 sensorMachine = Sensor()
 sensorMachine.name = 'sensor'
-mySM = sm.Cascade(sensorMachine, wallFollower())
-
-
-def frontWallPresent(frontDistance):
-    if frontDistance < desiredFront + tolerance:
-        return True 
-    return False
-
-
-def stickout(sensors):
-    if sensors[0] > maxRangeOfSensor and sensors[5] > maxRangeOfSensor:
+wallFollower = WallFollower()
+wallFollower.name = 'wallFollower'
+# mySM = sm.Cascade(sensorMachine, WallFollower())
+ 
+######################################################################
+#
+#            Running the robot & our custom functions
+#
+######################################################################
+ 
+ 
+def wallIsInfront(value):
+    if value < dDesired + tolerance:
         return True
-    # print sensors[0], sensors[5]
-    return False
-
-def stickoutLeft(sensors, oldSensors):
-    if sensors[0] > maxRangeOfSensor and oldSensors[0] > maxRangeOfSensor:
-        return True
-    # print sensors[0], sensors[5]
-    return False
-
-def stickoutRight(sensors, oldSensors):
-    if sensors[5] > maxRangeOfSensor and oldSensors[5] > maxRangeOfSensor:
-        return True
-    # print sensors[0], sensors[5]
-    return False
-
-
-def junctionCheck(sensors):
-    k=0
-    for i in sensors:
-        if i > 4:
-            k+=1
-    if k == 4:
+    else:
+        return False
+ 
+ 
+def front4red(sensors):
+    l = []
+    for i in sensors[1:5]:
+        if i >= maxSensorReading:
+            l.append(True)
+    if len(l) == 4:
         return True
     return False
+ 
+ 
+def left4red(sensors):
+    l = []
+    for i in sensors[0:4]:
+        if i >= maxSensorReading:
+            l.append(True)
+    if len(l) == 4:
+        return True
+    return False
+ 
+ 
+def right4red(sensors):
+    l = []
+    for i in sensors[2:6]:
+        if i >= maxSensorReading:
+            l.append(True)
+    if len(l) == 4:
+        return True
+    return False
+ 
+ 
+def junctionDetected(leftFront, leftDiag, rightFront, rightDiag, left, right):
+    if (leftFront >= maxSensorReading) and \
+            (leftDiag >= maxSensorReading) and \
+            (rightFront >= maxSensorReading) and \
+            (rightDiag >= maxSensorReading) or \
+            (left >= maxSensorReading or right >= maxSensorReading):
+        return True
+    return False
+ 
+ 
+def whereIsObstacle(right, left):
+    if right > left + tolerance:
+        return 'left'
+    elif left > right + tolerance:
+        return 'right'
+    else:
+        return 'right'
+ 
+ 
+# load data from a url
+def loadData(testUrl):
+    data = urllib2.urlopen(testUrl)
+    data = data.readlines()
+    data = [(k, int(v))
+            for k, v in [line.strip().split() for line in data]]
+    return data
+ 
+ 
+# map nodes
+# [left, up, right, down'] or [0,1,2,3]
+# which ever you prefer
+# (index-1)%4 -> gives the new left
+# (index+1)%4 -> gives the new right
+ 
+mapLv1 = {
+    'A': [None, None, None, 'J1'],
+    'B': [None, None, 'J1', None],
+    'C': [None, 'J1', None, None],
+    'X': ['J1', None, None, None],
+    'J1': ['B', 'A', 'C', 'X']
+}
+ 
+mapLv2 = {
+    'Z': [None, None, 'J1', None],
+    'X': ['J2', None, None, None],
+    'J1': ['Z', 'J4', 'J2', None],
+    'J2': ['J1', 'J3', 'X', None],
+    'J3': ['J4', 'B', 'A', 'J2'],
+    'J4': ['D', 'C', 'J3', 'J1'],
+    'A': ['J3', None, None, None],
+    'B': [None, None, None, 'J3'],
+    'C': [None, None, None, 'J4'],
+    'D': [None, None, 'J4', None]
+}
 
+level = mapLv2
 
+ 
+##########################################
+# search SM
+# Explanation to be added... hopefully...
+##########################################
+ 
+ 
+class genSearch(sm.SM):
+ 
+    startState = None
+    legalInputs = [0, 1, 2, 3]  # left, up, right, down
+ 
+    def __init__(self, goal, whichMap):
+        self.goal = goal
+        self.selectedMap = whichMap
+ 
+    def nextState(self, state, action):
+        return self.selectedMap[state][action]
+ 
+    def getNextValues(self, state, action):
+        nextState = self.nextState(state, action)
+        return nextState, nextState
+ 
+    def done(self, state):
+        return state == self.goal
+ 
+testUrl = "http://people.sutd.edu.sg/~oka_kurniawan\
+/10_009/y2015/2d/tests/level1_2.inp"
+ 
+# prepare the path lah, if not what?
+# max per trip is
+ 
+ 
+def preparePath(mapToUse):
+    data = loadData(testUrl)  # [('A', 10), ('B', 3), ('C', 15)]
+    path = []
+    pathTuple = [(a, ((b/6)+1)) for a, b in data]
+    for idx, val in enumerate(pathTuple):
+        # calculate forward path then calculate reverse path
+        # val[0] is goal, val[1] is number of trips
+        start = 'X'
+        goal = val[0]
+        forward = Search.smSearch(
+            genSearch(goal, mapToUse), start, depthFirst=False, DP=True)
+        reverse = Search.smSearch(
+            genSearch(start, mapToUse), goal, depthFirst=False, DP=True)
+        path.extend((forward + reverse) * val[1])
+    return path
+ 
+
+ 
 def setup():
     robot.gfx = gfx.RobotGraphics(drawSlimeTrail=False)
-    # robot.gfx.addStaticPlotSMProbe(y=('rightDistance', 'sensor',
-    #                                   'output', lambda x: x))
+    # wallFollower.path = preparePath(level)
+    wallFollower.path = [(None, 'X'), (0, 'J2'), (0, 'J1'), (1, 'J4'), (1, 'C'), (None, 'C'), (3, 'J4'), (2, 'J3'), (3, 'J2'), (2, 'X')]
+    mySM = sm.Cascade(sensorMachine, wallFollower)
     robot.behavior = mySM
     robot.behavior.start(traceTasks=robot.gfx.tasks())
  
